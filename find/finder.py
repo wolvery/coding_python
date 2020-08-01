f7697098acf3eac877f6bc2ea3ec5f8eff87b42b
WOLVERY <GUILHG2@GMAIL.COM>
2020-08-01 15:59:23 -0300

    Adds doc string

--- find/finder.py
+++ find/finder.py
@@ -3,30 +3,46 @@ from typing import Dict, List, Tuple
 
 ORIGINAL_NAME = 0
 SORTED_NAME = 1
+EMPTY = ""
 
 
 class Finder:
 
-    def __init__(self, string_list: List[str]):
+    def __init__(self, string_list: List[str]) -> None:
+        """
+        Finder search words which matches the same quantity of characters to a given word.
+        :param string_list: List of words to be searched on
+        :rtype: Finder
+        """
         self.conjunct = self.adjusts(string_list)
 
     @staticmethod
     def adjusts(string_list: List[str]) -> Dict[int, List[Tuple[str, str]]]:
+        """
+        Adjust a given string lists to a schema of dict[Length: sublist of words].
+        :param string_list: List of words to be adjusted
+        :rtype: dict[Length: sublist of words]
+        """
         conjunct = defaultdict(list)
 
         for element in string_list:
-            conjunct[len(element)] += [(element, sorted(element))]
+            conjunct[len(element)] += [(element, EMPTY.join(sorted(element)))]
 
         return conjunct
 
     def find(self, word: str) -> List[str]:
+        """
+        Generates a list of matched words to an input word.
+        :param word: Parameter to be searched on.
+        :rtype: List of matched words registered to this instance of Finder.
+        """
         matched_words: List[str] = list()
         length = len(word)
-        sorted_word = sorted(word)
+        sorted_word = EMPTY.join(sorted(word))
 
         for element in self.conjunct[length]:
             if sorted_word == element[SORTED_NAME]:
-                matched_words += element[ORIGINAL_NAME]
+                matched_words += [element[ORIGINAL_NAME]]
 
         return matched_words
 
--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,11 +1,11 @@
 import pytest
-from finder import Finder
-from typing import List, Dict, Tuple
+from find.finder import Finder
+from typing import Dict, List, Tuple
 
 
 @pytest.mark.parametrize("string_list, expected", [
-    (['asd', 'das'], {3: [('asd', 'asd'), ('das', 'asd')]}),
-    (['asd', 'das', 'asdd'], {3: [('asd', 'asd'), ('das', 'asd')], 4: [('asdd', 'asdd')]})
+    (['asd', 'das'], {3: [('asd', 'ads'), ('das', 'ads')]}),
+    (['asd', 'das', 'asdd'], {3: [('asd', 'ads'), ('das', 'ads')], 4: [('asdd', 'adds')]})
 
 ])
 def test_init_finder(string_list: List[str], expected: Dict[int, List[Tuple[str, str]]]):
@@ -16,18 +16,18 @@ def test_init_finder(string_list: List[str], expected: Dict[int, List[Tuple[str,
     assert all(instance.conjunct[length] == expected[length] for length in expected.keys())
 
 
-@pytest.mark.parametrize("string_list, input, expected", [
+@pytest.mark.parametrize("string_list, word, expected", [
     (['asd', 'asdd', 'fre', 'glk', 'lkm'], 'sad', ['asd']),
     (['complicated', 'happy', 'ppyha', 'sas', 'zzzz'], 'happy', ['happy', 'ppyha']),
     (['oom', 'oom', 'moo', 'omo'], 'omo', ['oom', 'oom', 'moo', 'omo']),
     (['sadapop', 'sada', 'pop', 'sasasasa', 'sasasasa'], 'zzz', []),
 ])
-def test_finder(string_list: List[str], input: str, expected: List[str]):
+def test_finder(string_list: List[str], word: str, expected: List[str]):
     """
     Test that finder.find collects the correct elements.
     """
 
     instance = Finder(string_list)
-    result = instance.find(input)
+    result = instance.find(word)
     # I can not guarantee if there is repeated
-    assert  sorted(result) == sorted(expected)
+    assert sorted(result) == sorted(expected)
